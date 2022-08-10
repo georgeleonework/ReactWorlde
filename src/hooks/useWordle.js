0b6ec1df692cb4b815bbf8b85574c29b9c66d979
georgeleonework <georgeleonework@gmail.com>
2022-08-10 14:49:28 -0400

    set up requirements to submit a valid guess

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -26,9 +26,22 @@ const useWordle = (solution) => {
     //track the current guess
     //add new guess when user hits enter
 
-    const handleKeyup = ({ key }) => {
-        console.log('key pressed - ', key)
-    
+    const handleKeyup = ({ key }) => { //fires any time that a user enters something on the keyboard
+        if (key === 'Enter') { //this would mean the user wants to submit the guess
+            //only add guess if turn is less than 5
+            if (turn > 5) {
+
+            }
+            //do not allow duplicate words
+            if (history.includes(currentGuess)) {
+
+            }
+            //check word is 5 chars long
+            if (currentGuess.length !== 5) {
+
+            }
+        }
+        
         if (key === 'Backspace') {
           setCurrentGuess(prev => prev.slice(0, -1))
           return
