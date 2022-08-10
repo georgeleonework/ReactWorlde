9c023f2c13316c860fdbc664061fbdc7548c9ac3
georgeleonework <georgeleonework@gmail.com>
2022-08-10 14:05:01 -0400

    set up keyup event listender

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -1,6 +1,16 @@
 import React from 'react'
+import { useEffect } from 'react'
+import useWordle from '../hooks/useWordle'
 
 export default function Wordle({ solution }) {
+    const { currentGuess, handleKeyup } = useWorlde(solution)
+
+    useEffect(() => {
+        window.addEventListender('keyup', handleKeyup)
+
+        return () => window.removeEventListener('keyup', handleKeyup)
+    })
+
   return (
     <div>Wordle</div>
   )
