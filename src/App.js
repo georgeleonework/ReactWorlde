bf87a8257f97b84ab94ce6e74e1b4e7fc435226c
georgeleonework <georgeleonework@gmail.com>
2022-08-10 13:26:55 -0400

    setting up port 3001 json server to communicate w port 300 wordle app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import './App.css'
+import React, { Component }  from 'react';
+import { useEffect , useState } from "react";
 
 function App() {
+  const [solution, setSolution] = useState(null)
+
+  useEffect(() => {
+    fetch('http://localhost:3001/solutions')
+      .then(res => res.json())
+      .then(json => {
+        console.log(json)
+      })
+  }, [])
+  
+
+
   return (
     <div className="App">
       <h1>Wordle (Lingo)</h1>
